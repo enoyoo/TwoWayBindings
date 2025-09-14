ef879a3d1fe447980e700d93c22eef3d4fa9a57f
Eno <enoyoo02@gmail.com>
2025-09-14 16:13:17 -0400

    initial commit

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,12 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var pressCount = 0
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColor: Color = .blue
+    @State private var selectedDate = Date()
+    @State private var stepperValue = 1
+    @State private var sliderValue = 50.0
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Text("Press Count: \(pressCount)")
+                .padding()
+                .animation(.interpolatingSpring, value: pressCount)
+            
+            Button("Press Me") {
+                pressCount += 1
+            }
+            .buttonStyle(.glassProminent)
+            
+            Spacer()
+            
+            TextField("Enter a name", text: $name)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Text("Name: \(name)")
+            
+            Spacer()
+            
+            Toggle("Toggle is \(toggleIsOn ? "on" : "off"):", isOn: $toggleIsOn)
+                .toggleStyle(.switch)
+                .padding()
+            
+            Spacer()
+            
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+            
+            ColorPicker("Pick a color:", selection: $selectedColor)
+                .padding()
+            
+            Spacer()
+            
+            DatePicker("Date", selection: $selectedDate)
+                .padding()
+            Text("Selected Date is \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+                .padding()
+            
+            Spacer()
+            
+            Stepper("Stepper value \(stepperValue)", value: $stepperValue, in: 1...10)
+                .padding()
+            
+            Spacer()
+            
+            Slider(value: $sliderValue, in: 0...100) {
+            } minimumValueLabel: {
+                Image(systemName: "speaker.minus")
+            } maximumValueLabel: {
+                Image(systemName: "speaker.plus")
+            }
+            .padding()
+            Text("Slider value: \(sliderValue.formatted(.number.precision(.fractionLength(1))))")
+            
+            Spacer()
+            
         }
         .padding()
     }
